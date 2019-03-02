96f5b9e5f302126976371388a848e114989583a3
Hitesh297 <hites_297@yahoo.co.in>
2019-03-02 22:13:54 +0530

    test

--- RunOpenCover.bat
+++ RunOpenCover.bat
@@ -1,4 +1,5 @@
 set nunitpath=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe
 set nunitpath_quotes="%nunitpath%"
+set WORKSPACEPATH = "%WORKSPACEESCAPED%"
 
-%WORKSPACEESCAPED%\\packages\\OpenCover.4.7.922\\tools\\OpenCover.Console.exe -register:user -target:%nunitpath_quotes% "-targetargs:%WORKSPACEESCAPED%\UnitTestProject1\bin\Release\UnitTestProject1.dll /ResultsDirectory:""c:\\Tools\\result"" /logger:trx" -output:c:\\Tools\\OpenCover.xml -mergebyhash
+%WORKSPACEPATH%\\packages\\OpenCover.4.7.922\\tools\\OpenCover.Console.exe -register:user -target:%nunitpath_quotes% "-targetargs:%WORKSPACEPATH%\UnitTestProject1\bin\Release\UnitTestProject1.dll /ResultsDirectory:""c:\\Tools\\result"" /logger:trx" -output:c:\\Tools\\OpenCover.xml -mergebyhash
