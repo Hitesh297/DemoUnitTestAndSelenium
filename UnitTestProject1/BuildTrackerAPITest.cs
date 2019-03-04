8ee0ee6fce796f0606fbce8926b80d8658608c97
Hitesh297 <hites_297@yahoo.co.in>
2019-03-05 03:08:54 +0530

    added tests

---
+++ UnitTestProject1/BuildTrackerAPITest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BuildTrackerAPI.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class BuildTrackerAPITest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DeploymentController deploymentController = new DeploymentController();
+            string result = deploymentController.Get(12);
+            Assert.AreEqual("value", result);
+        }
+    }
+}
