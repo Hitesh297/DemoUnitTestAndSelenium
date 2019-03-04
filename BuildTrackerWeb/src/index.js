a5aa1b0004d785a520a0e7188d9842736a1dbd8d
Hitesh297 <hites_297@yahoo.co.in>
2019-03-04 23:42:47 +0530

    initial commit- build tracker in react

---
+++ BuildTrackerWeb/src/index.js
@@ -0,0 +1,11 @@
+﻿import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    render() {
+        return (
+            <div>Hello World! </div>
+            );
+    }
+}
+ReactDOM.render(<App />, document.getElementById("index"));
